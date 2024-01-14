7340e836eeab269c50b7b838d7d035780e47e250
hydroEng <tuahaa.soomro@gmail.com>
2024-01-14 20:45:11 +1100

    Add print board function, fix bugs on column detection

--- script.js
+++ script.js
@@ -12,30 +12,42 @@ const gameboard = (function () {
         return false
     }
 
+    const display_board = () => {
+        for (i=0;i < board.length; i++) {
+            console.log(board[i])
+        }
+    }
+
     const place_marker = (row, col, marker) => {
+        var invalid_move = false
         // Ensure that x or o are the markers provided.
         if (!is_marker(marker)) {
             console.log("Error: Please only use 'x' or 'o' as markers.")
-            return
+            invalid_move = true
         }
 
         // Enforce that a, b are within values 1-3
         if (!(row > 0 && row < 4) || (!(col > 0 && col < 4))) {
             console.log("Error: Please provide values between 1 and 3 for rows and columns.")
-            return        
+            invalid_move = true        
         }
         
         board_value = board[row - 1][col - 1]
+        console.log(`Board value there is ${board_value}.`)
         // Exit if the selected board piece is already a marker.
-        if (is_marker(board_value)) {
+        invalid_move = is_marker(board_value)
+
+        if (invalid_move) {
+            console.log("Board not changed.")
+            display_board()
             return
         }
-
         // Place marker if all checks pass.
-        board[row].splice([col],1, marker)
+        board[row - 1].splice([col - 1],1, marker)
+        display_board()
     }
-    
 
-    return { board, place_marker }
+    
+    return { board, place_marker, display_board }
 }
 )()
